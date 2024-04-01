7d8cc7c199a95473a92f8a94b067e09a55592965
Ryan Seah <M150101@e.ntu.edu.sg>
2024-04-01 18:02:27 -0400

    Update __init__.py

--- matplottikz/__init__.py
+++ matplottikz/__init__.py
@@ -1,2 +1,3 @@
 from matplottikz.plotter import matplottikz
 from matplottikz.utils import marker_square, marker_pentagon, marker_triangle, marker_o, marker_star, marker_star_flipped, marker_diamond, marker_asterisk, marker_star, matplotlib_markers, empty_plot_dict, matplottikz_palette
+from matplottikz.utils import marker_o_plus, marker_plus, marker_x
